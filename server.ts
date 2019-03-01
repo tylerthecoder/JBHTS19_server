712a1abe3c3fae3bddf1a9286a28f1aa5b12875c
Tyler Tracy <me@tylertracy.com>
2019-03-01 15:49:03 -0600

    init commit

---
+++ server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import socketIO from "socket.io";
+import http from "http";
+import path from "path";
+
+const app = express();
+const server = new http.Server(app);
+const io = socketIO(server);
+
+app.get("/", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "../public/index.html"));
+});
+
+io.on("connection", function(socket) {
+  socket.emit("news", { hello: "world" });
+  socket.on("my other event", function(data) {
+    console.log(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server listening on port", 3000);
+});
